0220c5b2e0d103956a25448074f30bbb29419a7e
mensahdaniel <mensahdaniel5858910@gmail.com>
2023-07-14 16:31:55 +0100

    persistence

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,13 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	putchar('0');
 
-	for (digit1 = 0; digit1 <= 9; digit1++)
+	for (int i = 1; i < 10; i++)
 	{
-		for (digit2 = digit1; digit2 <= 9; digit2++)
-		{
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
-
-			if (digit1 != 9 || digit2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(',');
+		putchar(' ');
+		putchar(i + '0');
 	}
 
 	putchar('\n');
