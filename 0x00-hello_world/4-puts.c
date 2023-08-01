970c75f2c2948371c4914f37e8e9055acd60be9f
mensahdaniel <mensahdaniel5858910@gmail.com>
2023-08-01 06:11:14 +0100

    crackme

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stio.h>
+/**
+ * main - Aprogramme that prints a file with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle")
+
+
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char letter;
+	int i;
+	char letter;
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	}
 }
