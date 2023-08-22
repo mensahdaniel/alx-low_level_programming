4b3466a6058616562d33ec947b70c1976e61e38e
mensahdaniel <mensahdaniel5858910@gmail.com>
2023-08-22 10:44:46 +0100

    coreection

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stio.h>
+#include <stdio.h>
+
 /**
- * main - Aprogramme that prints a file with puts function
- * Return: 0 (success)
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle")
-
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
 
